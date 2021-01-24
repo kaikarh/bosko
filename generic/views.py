77d0ec24216275b7f2d766c095c2e72ec6836eb8
kai <kai.karhos@gmail.com>
2021-01-25 03:05:41 +0800

    use CF-Connecting-IP header to check

--- generic/views.py
+++ generic/views.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 def index(request):
     context = {
         "now": timezone.now(),
-        "remote_ip": request.headers.get('x_forwarded_for',
-                                         request.META['REMOTE_ADDR']),
+        "remote_ip": request.headers.get(
+            'CF-Connecting-IP',request.headers.get(
+                'x_forwarded_for', request.META['REMOTE_ADDR'])),
     }
     return render(request, 'generic/index.html', context)
