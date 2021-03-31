1596c395fe7cec86bd1bd0771411c5c095d3f4eb
kai <kai.karhos@gmail.com>
2021-03-31 15:00:41 +0800

    change db model char max_length

--- postmaker/models.py
+++ postmaker/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Release(models.Model):
     release_name = models.CharField(max_length=128, unique=True)
-    archive_name = models.CharField(max_length=128)
+    archive_name = models.CharField(max_length=200)
     archive_size = models.IntegerField(blank=True, default=0)
-    stream_song_name = models.CharField(max_length=128, blank=True)
+    stream_song_name = models.CharField(max_length=200, blank=True)
     stream_song_url = models.URLField(blank=True)
     share_link = models.URLField(blank=True)
     share_link_passcode = models.CharField(max_length=4, blank=True)
