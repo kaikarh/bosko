42f9ff02c39be0d7fc0a8aa94b9318dfb2400d8b
kai <kai.karhos@gmail.com>
2021-04-26 15:52:27 +0800

    fix dsca api url

--- dsca/urls.py
+++ dsca/urls.py
@@ -6,14 +6,10 @@ from . import views
 router = routers.DefaultRouter()
 router.register('', views.DaybookViewSet)
 
-urlpatterns = [
-    path('api/c', views.GeneralCreateView.as_view()),
-    path('access-api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
-
 app_name = "dsca"
 urlpatterns = [
     path('', views.index, name='index'),
     path('list-all/', views.ListAllView.as_view(), name='listall'),
+    path('api/c', views.GeneralCreateView.as_view()),
+    path('access-api/', include(router.urls)),
 ]
